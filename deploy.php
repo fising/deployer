850537cb0622023813a207027ac015ee9ebf03f6
fising <fising@qq.com>
2017-06-13 10:50:07 +0800

    待完善

--- deploy.php
+++ deploy.php
@@ -6,15 +6,16 @@ require 'recipe/common.php';
 
 set('repository', 'https://github.com/fising/coffer.git');
 set('git_tty', true);
-set('shared_files', ['prod.env']);
-set('shared_dirs', []);
-set('writable_dirs', []);
+set('shared_files', ['software/mac2222.md']);
+set('shared_dirs', ['upload']);
+set('writable_dirs', ['software']);
 set('allow_anonymous_stats', false);
+set('http_user', 'www');
 set('deploy_path', '/data/apps/coffer');
 
 desc('同步文件到生产服务器');
 task('rsync', function () {
-    run("rsync -av {{deploy_path}} root@127.0.0.1:/data/apps2/");
+    run("rsync -av --exclude=shared --delete {{deploy_path}} root@127.0.0.1:/data/apps2/");
 });
 
 desc('本地预部署项目');
@@ -26,6 +27,7 @@ task('deploy', [
     'deploy:shared',
     'deploy:writable',
     'deploy:clear_paths',
+    'rsync',
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
